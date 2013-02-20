c234e653400f97ec3e50c7f610a0bfc5cb01a72b
guopengpeng <595153538@qq.com>
2013-02-20 17:08:28 +0800

    Add the keyboard function

--- arch/arm/mach-omap2/board-panther.c
+++ arch/arm/mach-omap2/board-panther.c
@@ -762,7 +762,6 @@ static struct matrix_keymap_data board_map_data = {
 	.keymap_size		= ARRAY_SIZE(board_keymap),
 };
 
-
 static struct twl4030_keypad_data ynws_kp_data = {
 	.keymap_data	= &board_map_data,
 	.rows		= 6,
